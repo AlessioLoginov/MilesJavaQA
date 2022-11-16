7b6d4568c75c5d464e8a2bb3655080750d0ea55b
Alessio.Loginov <alekseyloginov79@gmail.com>
2022-11-16 06:57:29 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 50; //цена одного билета
+        int bonus = price / 20; //количество бонусных миль за полет
+        System.out.println("Количество начисленных миль за поездку равно = " + bonus); //выводим результат
+    }
+}
